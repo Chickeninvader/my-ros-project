1bd50d4c9f098a817e0acc49a5f87c72b3aea326
chickeninvader <vongocbachkhoa@gmail.com>
2024-11-07 17:27:43 -0700

    tutorial

--- launchers/my-subscriber.sh
+++ launchers/my-subscriber.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+rosrun my_package my_subscriber_node.py
 
 
 # ----------------------------------------------------------------------------
