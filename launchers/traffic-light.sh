fc93ee4e411de09cdb8db3f4ec8cb246e08a0a5a
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-11-13 11:25:24 -0700

    add trafficlight node

--- launchers/traffic-light.sh
+++ launchers/traffic-light.sh
@@ -5,8 +5,9 @@ source /environment.sh
 # initialize launch file
 dt-launchfile-init
 
-# launch subscriber
-rosrun traffic_light traffic_light_node.py
+# launch traffic light
+roslaunch traffic_light trafficlightnode.launch veh:=trafficlight001
+
 
 # wait for app to end
 dt-launchfile-join
